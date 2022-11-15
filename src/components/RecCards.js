7bee1f678971e27a095ef97ef345ab0ba10d9f76
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-11-15 15:45:31 -0500

    finished form, added routes to nav, started working on cards display

--- src/components/RecCards.js
+++ src/components/RecCards.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function RecCards (){
+function RecCards ({rec}){
+
+    const [isFrontCard, setIsFrontCard] = useState(true)
+
+    function handleOnClick (){
+        setIsFrontCard(!isFrontCard);
+    }
+
+
     return(
-        <h1>Rec Cards</h1>
+        <div onClick={handleOnClick}> 
+        {/* Front of Card */}
+
+            {isFrontCard ? 
+                <div className='front-card'> 
+                    <h2>{rec.name}</h2>
+                    <img src={rec.img} alt={rec.name}></img>
+                    <h4>{rec.area}, {rec.borough}</h4>
+                    <p>{rec.category}</p>
+                    <p>{rec.likes}</p>
+                </div>
+                : <div className='back-card'> 
+                    <h3>{rec.name}</h3>
+                    <h4>{rec.area}, {rec.borough}</h4>         
+                    <p>{rec.description}</p>
+                    <a href={rec.url}>Website</a> | <a href={rec.events}>Events</a>
+                    <p>Submitted By: {rec.user}</p>
+                </div>
+            }
+                
+
+
+        {/* Back of Card 
+            <h3>{rec.name}</h3>
+            <h4>{rec.area}, {rec.borough}</h4>         
+            <p>{rec.description}</p>
+            <a href={rec.url}>Website</a> | <a href={rec.events}>Events</a>
+            <p>Submitted By: {rec.user}</p>
+        */}
+        </div>
     )
 }
 
--- src/components/RecList.js
+++ src/components/RecList.js
@@ -1,2 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import NewRecForm from './NewRecForm';
+import RecCards from './RecCards';
+import Search from './Search';
 
+function RecList ({addNewRec, allRecs}){
+
+    const renderRecs = allRecs.map((rec) => {
+        return <RecCards key={rec.id} rec={rec} /> 
+    })
+
+    const [showForm, setShowForm] = useState(false)
+
+    function handleShowForm () {
+        setShowForm(!showForm)
+    }
+
+    return(
+        <div>
+            <h1>rec list</h1>
+            <Search />
+            <br></br>
+            <button onClick={handleShowForm}>Add New Rec</button>
+            {showForm ? <NewRecForm addNewRec={addNewRec} />  : null} 
+            <div className='card-list'>
+                {renderRecs}
+            </div>
+        </div>
+    )
+}
+
+export default RecList;
